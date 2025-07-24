2f148a8f8b61a82d2a9e92d42f9c0c63010d34a1
William Perron <hey@wperron.io>
2025-07-24 10:37:51 -0400

    add 500ms delay

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-Deno.serve((req) => {
+Deno.serve(async (req) => {
   let traceparent = req.headers.get("traceparent") ?? "unknown";
   console.log(`${req.method} ${req.url} traceparent: ${traceparent}`);
+  await new Promise((resolve, reject) => setTimeout(resolve, 500));
   return new Response(JSON.stringify(Deno.env.toObject()));
 });
