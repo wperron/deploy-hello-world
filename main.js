083438ab9fc392274ab0227434045738a30657fd
William Perron <hey@wperron.io>
2025-08-04 12:19:12 -0400

    Update main.js
    pretty print

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ Deno.serve(async (req) => {
   let traceparent = req.headers.get("traceparent") ?? "unknown";
   console.log(`${req.method} ${req.url} traceparent: ${traceparent}`);
   await new Promise((resolve, reject) => setTimeout(resolve, 500));
-  return new Response(JSON.stringify(Deno.env.toObject()));
+  return new Response(JSON.stringify(Deno.env.toObject(), null, 2));
 });
